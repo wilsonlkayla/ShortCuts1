e3dce4c9ea8d6807bb594edfe6aab960570c5fee
virgil23 <virgil17@live.com>
2018-07-05 20:38:27 -0400

    told me to commit

--- app/config/connection.js
+++ app/config/connection.js
@@ -7,7 +7,7 @@
   var Sequelize = require("sequelize");
 
   // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-  var sequelize = new Sequelize("appt_db", "root", "password", {
+  var sequelize = new Sequelize("appt_db", "root", "strpure", {
     host: "localhost",
     port: 3306,
     dialect: "mysql",
