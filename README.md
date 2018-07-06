f8af0252456fc827f42845b2b8ed14a5a8b1ca0b
Seirradawson <seirradawson@Seirra-Dawsons-MacBook-Pro.local>
2018-07-05 20:16:20 -0400

    Update README.md and customer keywords

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# ShortCuts
+# Shortcuts
+
+### Overview
+This application was created to assist the user with scheduling an appointment with their local barbershop or beautician. The user will have the ability to schedule his/her appointment and see who is currently waiting in the queue to get hair service.
+
+<!-- Heroku link here -->
+
+### Technologies Used
+  * HTML
+  * CSS
+  * JavaScript
+  * Node.js
+  * Express.js
+  * Package.JSON
+  * NPM Modules
+  * Bulma
+  * mySQL
+  * Sequelize
+  * Heroku
+
+### Authors
+  * Kayla Wilson
+  * Seirra Dawson
+  * Virgil Baez
--- app/models/customer.js
+++ app/models/customer.js
@@ -6,7 +6,7 @@ var Sequelize = require("sequelize");
 // sequelize (lowercase) references our connection to the DB.
 var sequelize = require("../config/connection.js");
 
-// Creates a "Chirp" model that matches up with DB
+// Creates a "Customer" model that matches up with DB
 var Customer = sequelize.define("Customer", {
   // Their Name
   customer_name: Sequelize.STRING,
@@ -22,5 +22,5 @@ var Customer = sequelize.define("Customer", {
 // Syncs with DB
 Customer.sync();
 
-// Makes the Chirp Model available for other files (will also create a table)
+// Makes the Customer Model available for other files (will also create a table)
 module.exports = Customer;
